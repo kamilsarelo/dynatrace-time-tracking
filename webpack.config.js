8e78f462c1b781c8933d77f0a36deb7ae3f2cd68
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2020-04-24 16:23:53 +0200

    Add tsconfig.json and a webpack configuration for transpiling the TypeScript code back to JavaScript
    (cherry picked from commit e074badaa7856072083826baff90c547ed703d91)

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "bookmarklet/bookmarklet.ts"),
+  output: {
+    filename: "bookmarklet.js",
+    path: path.resolve(__dirname, "bookmarklet/dist"),
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "bookmarklet.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+    ],
+  },
+  // resolve: {
+  //   extensions: [".ts", ".css"],
+  // },
+  devtool: "source-map",
+};
